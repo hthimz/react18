cf2a2d87130037129bcafebbbf1b08baca2abbb4
Himanshu Tomar <himanshutomar@Himanshus-MacBook-Pro.local>
2023-04-26 13:43:06 +0530

    Updates

---
+++ index.6bd02f5a.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div", {
+    id: "container"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "Heading 1"),
+    React.createElement("h1", {}, "Heading 2")
+]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
